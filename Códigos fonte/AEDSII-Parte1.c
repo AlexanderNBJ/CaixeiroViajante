17fbe34d6ba80a0384f814e4c3510e02e373230b
Alexander Neves <168145399+AlexanderNBJ@users.noreply.github.com>
2024-09-27 18:03:59 -0300

    Add files via upload

---
+++ Códigos fonte/AEDSII-Parte1.c
@@ -0,0 +1,218 @@
+/*
+    3ª Avaliação de AEDS II - Trabalho Prático
+    O Problema do Caixeiro Viajante - Força Bruta
+
+    Alexander Neves Barbosa Júnior
+    Data: 08/08/2024
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+// define uma variável global de comparações
+int COMPARACOES = 0;
+
+/*
+    Bloco responsável pala criação de uma matriz simétrica com elementos aleatórios
+    É uma função chamada pela função inicializaMatriz
+    Note que são gerados números de 1 a 9, a menos da diagonal principal
+*/
+
+int **criaMatrizDeAdjascencias(int tamanho){
+    int **matriz = (int**) malloc(tamanho*sizeof(int*));
+
+    for(int i=0; i<tamanho; i++){
+        matriz[i] = (int*) malloc(tamanho*sizeof(int));
+        for(int j=0; j<tamanho; j++){
+            if(j==i){
+                matriz[i][j]=0;
+            }
+            else{
+                if(i<j){
+                    matriz[i][j]=rand()%9 +1;
+                }
+                else{
+                    matriz[i][j]=matriz[j][i];
+                }
+            }
+        }
+    }
+
+    return matriz;
+}
+
+/*
+    Bloco responsável pala inicialização de uma matriz simétrica com elementos aleatórios
+    Chama a função criaMatrizDeAdjascencias e define o valor do tamanho da matriz
+*/
+int **inicializaMatriz(int *tamanho){
+    srand(time(0));
+    printf("Digite o tamanho da matriz: ");
+    scanf("%i", tamanho);
+
+    while(*tamanho<2){
+        printf("Por favor, digite um tamanho de, no mínimo, 2: ");
+        scanf("%i", tamanho);
+    }
+    
+    return criaMatrizDeAdjascencias(*tamanho);
+}
+
+/*
+    Função responsável por imprimir a matriz no terminal
+*/
+void imprimeMatriz(int **matriz, int tamanho){
+    printf("\n");
+    for(int i=0; i<tamanho; i++){
+        for(int j=0; j<tamanho; j++){
+            printf("%i ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return;
+}
+
+/*
+    Função responsável pelo cálculo do custo do caminho atual
+*/
+int calculaCusto(int **matriz, int *caminho, int tamanho) {
+    int custo = 0;
+
+    // o caminho percorrido na matriz depende da relação de dois elementos consecutivos no vetor caminho
+    for (int i = 0; i < tamanho - 1; i++) {
+        custo += matriz[caminho[i]][caminho[i + 1]];
+    }
+    
+    // soma o custo de voltar ao primeiro vértice da matriz por meio do último elemento do caminho
+    custo += matriz[caminho[tamanho - 1]][caminho[0]];
+    return custo;
+}
+
+void trocar(int *x, int *y) {
+    int aux = *x;
+    *x = *y;
+    *y = aux;
+}
+
+/*
+    Função que auxilia nas permutações, invertendo o caminho
+*/
+void trasPraFrente(int *inicio, int *fim) {
+    while ((inicio != fim) && (inicio != --fim)) {
+        trocar(inicio++, fim);
+    }
+}
+
+/*
+    Função que gera a próxima permutação, chamando as funções de troca e de inverter de trás pra fernte
+    Ela permita os elementos no próprio vetor, evitando criar diversos vetores auxiliares
+*/
+int fazProximaPermutacao(int *inicio, int *fim) {
+    int *fimDoVetor = fim;
+
+    // verifica se está na última permutação possível
+    if (inicio == fim){
+        return 0;
+    }
+
+    if (inicio == --fimDoVetor){
+        return 0;
+    } 
+
+    // realiza as permutações
+    while (1) {
+        int *elementoAtual = fimDoVetor;
+
+        if (*--fimDoVetor < *elementoAtual) {
+            int *sucessor = fim;
+
+            while (!(*fimDoVetor < *--sucessor));
+
+            trocar(fimDoVetor, sucessor);
+            trasPraFrente(elementoAtual, fim);
+
+            return 1;
+        }
+        if (fimDoVetor == inicio) {
+            trasPraFrente(inicio, fim);
+            return 0;
+        }
+    }
+}
+
+/*
+    Função responsável por realizar todas as permutações possíveis e analisar o custo de cada
+*/
+int *forcaBrutaCaixeiroViajante(int **matriz, int tamanho, int *menorCusto) {
+    int caminho[tamanho];
+    int *melhorCaminho= (int*) malloc((tamanho+1)*sizeof(int));
+
+    for (int i = 0; i < tamanho; i++) {
+        caminho[i] = i;
+    }   
+
+    // Gerar todas as permutações do caminho, avaliando qual é o menor custo dentre elas
+    do {
+        int custoAtual = calculaCusto(matriz, caminho, tamanho);
+        COMPARACOES+=1;
+
+        // salva o menor custo e grava o melhor caminho
+        if (custoAtual < *menorCusto) {
+            *menorCusto = custoAtual;
+
+            for(int i=0; i< tamanho; i++){
+                melhorCaminho[i]=caminho[i];
+            }
+            melhorCaminho[tamanho]=melhorCaminho[0];
+        }
+
+    } while (fazProximaPermutacao(caminho, caminho + tamanho));
+
+    // retorna o melhor caminho, umas vez que o menor custo foi alterado por meio do ponteiro
+    return melhorCaminho;
+}
+
+int main(){
+    int tamanho, menorCusto, **matriz;
+    int tempo = time(0);
+    clock_t begin, end;
+
+    // cria a matriz de adjascências aleatória e assume o custo como "infinito"
+    matriz = inicializaMatriz(&tamanho);
+    menorCusto = INT_MAX;
+
+    imprimeMatriz(matriz, tamanho);
+    printf("\nEssa é a matriz a ser analisada por força bruta.\n");
+    
+    // chama a função responsável pelo cálculo e imprime os resultados obtidos
+    begin = clock();
+    int *melhorCaminho = forcaBrutaCaixeiroViajante(matriz, tamanho, &menorCusto);
+    printf("\nO custo mínimo para esse problema do Caixeiro Viajante é de: %i\n", menorCusto);
+
+
+    // Imprime o caminho de menor custo (o print está dessa forma para melhorar na visualização do caminho)
+    printf("Dessa forma, o caminho ideal é:\n");
+
+    for(int i=0; i < tamanho+1; i++){
+        if(i==tamanho){
+            printf("%i",melhorCaminho[i]);
+        }
+        else{
+            printf("%i-->",melhorCaminho[i]);
+        }
+    }
+
+    // imprime a quantidade de comparações realizadas, que é da ordem de n!
+    printf("\n%i comparações realizadas", COMPARACOES);
+
+    end = clock();
+    printf("\nPrograma executado em %.lf ms\n", ((double)end - begin) / ((CLOCKS_PER_SEC / 1000)));
+
+    free(matriz);
+    free(melhorCaminho);
+    
+    return 0;
+}
---
+++ Códigos fonte/AEDSII-Parte2.c
@@ -0,0 +1,201 @@
+/*
+    3ª Avaliação - Trabalho Prático
+    O Problema do Caixeiro Viajante - Vizinho Mais Próximo
+
+    Alexander Neves Barbosa Júnior
+    Data: 08/08/2024
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <limits.h>
+
+// define uma variável global de comparações
+int COMPARACOES = 0;
+
+// retorna 1 se as strings forem iguais e 0 caso contrário
+int stringsIguais(char *string1, char *string2, int tamanho){
+    for(int i=0; i<tamanho; i++){
+        if(string1[i]!=string2[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// inicializa a matriz de acordo com o arquivo, preservando a natureza da diagonal da matriz
+int **criaMatrizDeAdjascencias(FILE *arquivo, int **matriz, int *tamanho, int *tipoMatriz, char *nomeDoCaso){
+    char stringAux1[100];
+    char stringAux2[100];
+
+    while(fscanf(arquivo, "%s :", stringAux1)!=EOF){
+
+        // acha o nome do caso
+        if(stringsIguais(stringAux1, "NAME", 4)){
+            fscanf(arquivo, "%s\n", stringAux2);
+            strcpy(nomeDoCaso, stringAux2);
+        }   
+
+        // acha o tamanho da matriz e aloca ela
+        if(stringsIguais(stringAux1, "DIMENSION", 9)){
+            fscanf(arquivo, "%i", tamanho);
+
+            matriz = (int**) malloc(*tamanho*sizeof(int*));
+
+            for(int i=0; i<*tamanho; i++){
+                matriz[i] = (int*) malloc(*tamanho*sizeof(int));
+            }
+
+        }
+
+        // descobre se é matris diagonal superior (1) ou inferior (0)
+        if(stringsIguais(stringAux1, "EDGE_WEIGHT_FORMAT", 18)){
+            fscanf(arquivo, "%s\n", stringAux2);
+
+            if(stringsIguais(stringAux2,"UPPER_DIAG_ROW",14)){
+                *tipoMatriz = 1;
+            }
+            else{
+                *tipoMatriz = 0;
+            }
+        }
+
+        // começa a colocar os elementos na matriz e transpõe para completar a matriz
+        if(stringsIguais(stringAux1, "EDGE_WEIGHT_SECTION", 19)){
+            if(*tipoMatriz == 1){
+                for(int i=0; i<*tamanho; i++){
+                    for(int j=i; j<*tamanho; j++){
+                        fscanf(arquivo, "%i", &matriz[i][j]);
+                    }
+                }
+
+                for(int i=0; i<*tamanho; i++){
+                    for(int j=0; j<*tamanho; j++){
+                        if(i<j){
+                            matriz[j][i]=matriz[i][j];
+                        }
+                    }
+                }
+            }
+            else{
+                for(int j=0; j<*tamanho; j++){
+                    for(int i=j; i<*tamanho; i++){
+                        fscanf(arquivo, "%i", &matriz[i][j]);
+                    }
+                }
+
+                for(int i=0; i<*tamanho; i++){
+                    for(int j=0; j<*tamanho; j++){
+                        if(i>j){
+                            matriz[j][i]=matriz[i][j];
+                        }
+                    }
+                }
+            }
+            break;
+        }
+    }
+    return matriz;
+}
+
+/*
+    Função responsável pela busca do vizinho mais próximo (i) na linha atual
+*/
+int acharvizinho(int atual, int n, int visitado[], int ** matriz) {
+    int minDist = INT_MAX;
+    int maisprox = -1;
+    
+    for (int i = 0; i < n; i++) {
+        if (!visitado[i] && i != atual) {
+            int dist = matriz[atual][i];
+            if (dist < minDist) {
+                minDist = dist;
+                maisprox = i;
+            }
+        }
+    }
+    
+    return maisprox;
+}
+
+/*
+    Função responsável por criar o caminho do Vizinho Mais Próximo, mantendo o controle por meio do vetorVisitados
+*/
+void vizinhoMaisProximo(int **matriz, int tamanho, int *caminho, int *custo){
+    int *vetorVisitados = (int*) calloc(tamanho, sizeof(int));
+    int atual = 0;
+    int contador = 1;
+    vetorVisitados[atual] = 1;
+    
+    for (int i = 1; i < tamanho; i++) {
+        int maisprox = acharvizinho(atual, tamanho, vetorVisitados, matriz);
+        COMPARACOES++;
+
+        vetorVisitados[maisprox] = 1;
+        *custo+=matriz[i][maisprox];
+        caminho[contador] = maisprox;
+        contador++;
+        atual = maisprox;
+    }
+    
+    caminho[contador++] = caminho[0];
+    free(vetorVisitados);
+
+    return;
+}
+
+/*
+    Para a leitura dos dados de cada caso no Moodle, foi considerado que o arquivo está na mesma pasta que esse fonte.
+*/
+int main(){
+    FILE *arquivo;
+    int **matriz;
+    int tipoMatriz;
+    int tamanho;
+    int custo = 0;
+    char nomeDoCaso[20];
+    clock_t begin, end;
+    
+    printf("Digite o nome do arquivo (com a extensão): ");
+    scanf("%s", nomeDoCaso);
+    arquivo = fopen(nomeDoCaso,"r");
+
+    if(!arquivo){
+        printf("Arquivo não encontrado");
+        return 0;
+    }
+
+    // lê o arquivo
+    matriz = criaMatrizDeAdjascencias(arquivo, matriz, &tamanho, &tipoMatriz, nomeDoCaso);
+
+    int* caminho = (int*) calloc((tamanho+1), sizeof(int));
+
+    // realiza o algoritmo, marcando o tempo necessário para isso
+    begin = clock();
+    vizinhoMaisProximo(matriz, tamanho, caminho, &custo);
+    end = clock();
+    
+    // imprime as respostas
+    printf("\n--------------O problema em questão é o problema %s--------------\n\n", nomeDoCaso);
+    printf("Dessa forma, o caminho ideal é:\n\n");
+
+    for(int i=0; i < tamanho+1; i++){
+        if(i==tamanho){
+            printf("%i\n\n",caminho[i]);
+        }
+        else{
+            printf("%i-->",caminho[i]);
+        }
+    }
+
+    printf("O custo total para percorrer esse caminho é de %i\n", custo);
+    printf("A quantidade de comparações realizada foi de %i\n", COMPARACOES);
+    printf("Programa executado em %.lf ms\n", ((double)end - begin) / ((CLOCKS_PER_SEC / 1000)));
+    fclose(arquivo);
+    free(matriz);
+    free(caminho);
+
+    return 0;
+}
